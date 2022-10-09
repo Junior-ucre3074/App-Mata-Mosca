12889d2d5f6b0be531b29cb1d25554871fd52b83
Junior-ucre-3074 <dimenofc.21go@gmail.com>
2022-10-09 20:30:25 -0300

    criando funçao cronometro

--- js/jogo.js
+++ js/jogo.js
@@ -1,6 +1,8 @@
 
 let altura = 0;
 let largura = 0;
+let vidas = 1;
+let tempo = 10;
 
 function gameScreen() {
     altura = window.innerHeight;
@@ -11,13 +13,39 @@ function gameScreen() {
 
 gameScreen();
 
+let cronometro = setInterval(function(){
+    
+    tempo -=1;
+    if(tempo < 0){
+        clearInterval(cronometro);
+        clearInterval(criaMosca);
+        alert('vitoria');
+    }else{
+        document.getElementById('cronometro').innerHTML = tempo;
+    }
+    
+   
+    
+    
+},1000);
+
 function randomPosition() {
     let posicaoX = Math.floor(Math.random() * largura) - 90;
     let posicaoY = Math.floor(Math.random() * altura) - 90;
     let flyEx = document.getElementById('fly');
+    let life = document.getElementById('v' + vidas);
 
     if(flyEx){
         flyEx.remove();
+        if(vidas > 3){
+            window.location.href = 'game_over.html';
+        }else{
+            life.src = "./img/emptyHeart.png"
+
+             vidas ++;
+        }
+
+       
     }
 
     posicaoX = posicaoX < 0 ? 0 : posicaoX;
@@ -33,6 +61,9 @@ function randomPosition() {
     fly.style.top = posicaoY + 'px';
     fly.style.position = 'absolute';
     fly.id = 'fly';
+    fly.onclick = function(){
+        this.remove();
+    }
 
     document.body.appendChild(fly);
 
